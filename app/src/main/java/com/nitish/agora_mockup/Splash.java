d1310cc648527fd6d8bb8c4c4f41d7d585139243
NitishGadangi <nitishgadangi@gmail.com>
2020-04-09 18:29:34 +0530

    LOGIN

---
+++ app/src/main/java/com/nitish/agora_mockup/Splash.java
@@ -0,0 +1,15 @@
+package com.nitish.agora_mockup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
