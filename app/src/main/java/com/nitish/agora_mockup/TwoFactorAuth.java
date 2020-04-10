2105bb191c2ad156bfa4e2b6ecabdd19c29ddaff
NitishGadangi <nitishgadangi@gmail.com>
2020-04-10 13:10:57 +0530

    Forgot Password & TwoFactorAuth

---
+++ app/src/main/java/com/nitish/agora_mockup/TwoFactorAuth.java
@@ -0,0 +1,14 @@
+package com.nitish.agora_mockup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TwoFactorAuth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_factor_auth);
+    }
+}
